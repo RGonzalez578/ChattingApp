856e78fd7c974ae517be99128d2d2e6bb1c1788e
Ronald Chaves <ronaldchavesgonzalez@gmail.com>
2023-10-07 18:19:07 -0600

    Initial Commit

---
+++ lib/login_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+// Use 'stl' shortcut to create a class that extends from StateLessWidget
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('New Telegram'),
+      ),
+      drawer: Drawer(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('Button pressed');
+        },
+      ),
+      body: Column(
+        children: [
+          Text('Let\' sign you in!',
+              style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.black54,
+                  fontWeight: FontWeight.w700,
+                  letterSpacing: 0.5)),
+          Text(
+            'Welcome back \n You\'ve been missed',
+            style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.normal,
+                color: Colors.black54),
+          ),
+          Image.network(
+            'https://static.wikia.nocookie.net/stardewvalley/images/8/8c/Leah-Portrait_192px.png/revision/latest/thumbnail/width/360/height/360?cb=20160306051756',
+            height: 150,
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.blue,
+            child: FlutterLogo(),
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.all(50),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:chat_app/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ChatApp());
+}
+
+class ChatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'New Telegram',
+        theme: ThemeData(primarySwatch: Colors.indigo, fontFamily: 'RedHatDisplay'),
+        home: LoginPage());
+  }
+}
